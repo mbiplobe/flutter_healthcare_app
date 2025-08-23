9a5dadd6db632b3ccac9031123ff342c8ca6805b
Mujahid Islam <mbiplobe@gmail.com>
2025-08-23 16:07:22 +0600

    Root has been upgraded

--- lib/src/config/route.dart
+++ lib/src/config/route.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_healthcare_app/src/model/doctor.dart';
 import 'package:flutter_healthcare_app/src/pages/detail_page.dart';
 import 'package:flutter_healthcare_app/src/pages/doctor_consultant_page.dart';
 import 'package:flutter_healthcare_app/src/pages/splash_page.dart';
 import 'package:go_router/go_router.dart';
 
-
 final GoRouter router = GoRouter(
   routes: [
     GoRoute(
       path: AppRoutes.splashRoute,
-      builder: (context, state) =>  SplashPage(),
+      builder: (context, state) => SplashPage(),
     ),
     GoRoute(
       path: AppRoutes.homeRoute,
       builder: (context, state) => DoctorConsultantPage(),
     ),
-
-     GoRoute(
-      path: '/DetailPage',
+    GoRoute(
+      path: AppRoutes.detailsRoute,
       builder: (context, state) {
-        final doctor = state.extra;
+        final doctor = state.extra as Doctor; // cast if you know the type
         return DetailPage(doctor: doctor);
       },
     ),
@@ -27,7 +26,7 @@ final GoRouter router = GoRouter(
   errorBuilder: (context, state) {
     return Scaffold(
       body: Center(
-        child: Text(' ${state.error}'),
+        child: Text('${state.error}'),
       ),
     );
   },
@@ -35,6 +34,6 @@ final GoRouter router = GoRouter(
 
 class AppRoutes {
   static const splashRoute = "/";
-  static const homeRoute = '/HomePage';
+  static const homeRoute = "/HomePage";
   static const detailsRoute = "/DetailPage";
 }
